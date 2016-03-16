ae42ab440696b8e75920b4fc10f897b3ad65dd23
Jim Chiang <jchiang@slac.stanford.edu>
2016-03-16 11:53:21 -0700

    for consistency, use .ang SED files and wave_type='Ang' in galsim.SED call

--- scripts/galsim_baker.py
+++ scripts/galsim_baker.py
@@ -30,7 +30,7 @@ class GalSimPhotometry(object):
             self.bandpasses[band] = bp.withZeropoint('AB', _effective_diameter,
                                                      _exptime)
 
-        # Read the SED filenames for the wavelengths in nm.
+        # Read the SED filenames.
         self.sed_dir = os.path.join(photometry_baker_dir, 'data', 'sed')
         self.sed_names = sorted([x for x in os.listdir(self.sed_dir)
                                  if 'spec' in x and 'ang' not in x])
@@ -40,11 +40,13 @@ class GalSimPhotometry(object):
         Return the ug, gr, ri, iz, zy colors and execution times for
         the specifed SED and redshift.
         """
-        sed_fn = os.path.join(self.sed_dir, sed_name)
-        sed = galsim.SED(sed_fn).atRedshift(redshift)
+        # Use the SED files that have the wavelength values converted
+        # to Angstroms since the flux units are erg/cm^2/s/A.
+        sed_fn = os.path.join(self.sed_dir, sed_name + ".ang")
+        sed = galsim.SED(sed_fn, wave_type='Ang').atRedshift(redshift)
         tstart = time.time()
         my_mags = []
-        for band, bp in self.bandpasses.items():
+        for bp in self.bandpasses.values():
             flux = sed.calculateFlux(bp)
             my_mags.append(-2.5*np.log10(flux) + bp.zeropoint)
         time_spent = time.time() - tstart
